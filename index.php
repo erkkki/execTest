06f0b230cd64000be265d1bd79abcadd6f70d5d2
erkkki <erkheikki@hotmail.com>
2021-11-01 23:34:44 +0200

    Button example.

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
 <html lang="en">
 <head>
     <title>Exec test</title>
+    <script>
+        // Open link function.
+        const goto = (link) => {
+          window.location.href = link;
+        }
+    </script>
 </head>
 <body>
     <a href="./commands/calc.php">Launch calculator</a>
+
+    <!-- html button open link -->
+    <button onclick="goto('./commands/calc.php')">Launch calculator</button>
 </body>
 </html>
